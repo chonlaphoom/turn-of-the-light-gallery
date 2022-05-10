52bd95900e90805b90364ebb2bffea1131f335d2
chonlaphoom <chonlaphoomth@gmail.com>
2022-05-10 22:02:55 +0700

    add bat

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -1,6 +1,10 @@
 
 const AboutPage = () => {
-    return <></>
+    return <>
+        <div>
+            <img src="https://media1.giphy.com/media/0xR7MUO0hJfWtco7C6/giphy.gif" width={100} height={100}/>
+        </div>
+    </>
 }
 
 export default AboutPage;
